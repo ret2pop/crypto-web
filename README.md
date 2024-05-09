ab3252da3a55e6c5be0acdba42f72b89157809f3
Preston Pan <preston@nullring.xyz>
2024-05-08 17:56:07 -0700

    add k8s stuff; probably doesn't work

--- README.md
+++ README.md
@@ -9,4 +9,9 @@ Run these commands within the project root (installing with docker):
 docker image build -t python:0.0.1 ./
 docker run -p [host port]:8888 python:0.0.1
 ```
-alternatively, just run the python script.
+alternatively, just run the python script. Make sure to make an `apikey.txt` file in the project root containing an
+https://www.alphavantage.co/ api key, or provide an APIKEY environment variable to the docker image.
+## For the Record
+I have no idea how k8s works; I read the documentation and then hacked together everything in the k8s/ folder. I don't
+have a real way to test it as I don't have my own k8s node cluster, and so if I messed this up somehow at least you
+understand the logic.
--- main.py
+++ main.py
@@ -5,16 +5,25 @@ import os
 
 
 app = Flask(__name__)
-with open("apikey.txt", "r") as f:
+dirname = os.path.dirname(__file__)
+filename = os.path.join(dirname, './secret/apikey.txt')
+
+with open(filename, "r") as f:
     apikey = f.read()
-CURRENCY = os.environ.get('CURRENCY', "BTC")
-MARKET = os.environ.get('MARKET', 'EUR')
-APIKEY = os.environ.get('APIKEY', apikey)
+CURRENCY = os.environ.get('CURRENCY', "BTC").strip()
+MARKET = os.environ.get('MARKET', 'EUR').strip()
+APIKEY = os.environ.get('APIKEY', apikey).strip() # don't actually use the environ.get; use a k8s secret
+print(MARKET)
+print(CURRENCY)
 
 @app.route('/')
 @app.route('/index', methods=['GET'])
 def index():
     re = requests.get(f"https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_DAILY&symbol={CURRENCY}&market={MARKET}&apikey={APIKEY}")
+
+    if 'Information' in re.json():
+        return "<h1>ERROR</h1><p>API key request limit exceeded</p>"
+
     data = re.json()['Time Series (Digital Currency Daily)'].items()
     string = "<h1>Historical Time Series (BTC)</h1><ul>"
     for item in data:
@@ -27,4 +36,3 @@ def index():
 
 if __name__ == '__main__':
    app.run(host="0.0.0.0", port=8888)
-# print(data.keys())
